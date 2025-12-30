6b81e10b5203675a2f14b234a3fb9cb3ba54c3b3
Level-pixel101 <theinfinate1001@gmail.com>
2025-12-30 23:25:02 +0300

    fixed file not found errror

--- Target_cursor/src/App.jsx
+++ Target_cursor/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import TargetCursor from './TargetCursor';
+import TargetCursor from './components/TargetCursor.jsx';
 import './App.css';
 
 export default function App() {
